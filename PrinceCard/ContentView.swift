e728f1ac6b05c6c53c33e927bf25c9a678366794
PrynsTag <princevelasco16@gmail.com>
2021-06-03 13:49:14 +0800

    Initial Commit

---
+++ PrinceCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PrinceCard
+//
+//  Created by Prince Carl Velasco on 6/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PrinceCard/PrinceCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrinceCardApp.swift
+//  PrinceCard
+//
+//  Created by Prince Carl Velasco on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PrinceCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
