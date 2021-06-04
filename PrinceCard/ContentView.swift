b62c28f8b6931d5bffb6dc5b8ea1d2de1fc2eeb6
PrynsTag <princevelasco16@gmail.com>
2021-06-04 21:36:09 +0800

    Initial Commit

--- PrinceCard/ContentView.swift
+++ PrinceCard/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemTeal).ignoresSafeArea(.all)
+            VStack {
+                Image("prince")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Prince Velasco")
+                    .colorInvert()
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                Text("Mobile Developer")
+                    .colorInvert()
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "(+63)946 165 3691", imageName: "phone.fill")
+                InfoView(text: "princevelasco16@gmail.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 12 Pro Max")
     }
 }
---
+++ PrinceCard/InfoView.swift
@@ -0,0 +1,31 @@
+//
+//  InfoView.swift
+//  PrinceCard
+//
+//  Created by Prince Carl Velasco on 6/4/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50)
+            .colorInvert()
+            .overlay(HStack {
+                Image(systemName: imageName).accentColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill").previewLayout(.sizeThatFits)
+    }
+}
